c12e6539f7704caf35abab5133e8e4de71ab9a46
Arif.A <130204173+arifaslam2002@users.noreply.github.com>
2025-09-19 19:42:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {connectDB} from './lib/db.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 job.start();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use("/api/auth",authRoutes)
 app.use("/api/book",bookRoutes)
